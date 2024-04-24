73597f28f64d957f48270ea4367453c6fb1b6440
altasheheth <mweumartin@gmail.com>
2024-04-25 00:50:27 +0300

    week 2 assignment, web page with images and links

---
+++ JavaScript/index.js
@@ -0,0 +1,23 @@
+
+// Using var
+//Keyword variable_name  = value;
+//var name = "Martty."
+//console.log(name);
+
+//Using let
+//let location = "Desai";
+//console.log(location);
+
+//Using const
+//const Age = 16;
+//console.log(Age);
+
+
+//naming convention in js
+
+//camelcase
+
+
+
+//snake_case
+var last_name;
